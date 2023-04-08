0ba05acec2f11fca589a64fc655e7d7a9a13c03f
Yana <yana@tkh.nz>
2023-04-08 15:40:04 +1200

    Make sure DataContext cleans up between tests and add tests for GetTodoItem

--- Backend/TodoList.Api/TodoList.Api.UnitTests/TodoItemsControllerShould.cs
+++ Backend/TodoList.Api/TodoList.Api.UnitTests/TodoItemsControllerShould.cs
@@ -11,54 +11,137 @@ using Xunit;
 
 namespace TodoList.Api.UnitTests
 {
-    public class TodoItemsControllerShould
+  public class TodoItemsControllerShould
+  {
+
+    private static TodoContext InitContext()
     {
-        [Fact]
-        public async Task Return_AllIncompleteItems_When_GetTodoItems()
-        {
-            var _contextOptions = new DbContextOptionsBuilder<TodoContext>()
-                .UseInMemoryDatabase(nameof(TodoItemsController))
-                //.ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning))
-                .Options;
+      var _contextOptions = new DbContextOptionsBuilder<TodoContext>()
+        .UseInMemoryDatabase(nameof(TodoItemsController))
+        //.ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+        .Options;
 
-            var context = new TodoContext(_contextOptions);
+      var context = new TodoContext(_contextOptions);
 
-
-            context.TodoItems.Add(new TodoItem
-            {
-                Id = Guid.NewGuid(),
-                IsCompleted = false,
-                Description = "Pending 1"
-            }); 
-            context.TodoItems.Add(new TodoItem
-            {
-                Id = Guid.NewGuid(),
-                IsCompleted = true,
-                Description = "Completed"
-            });
-            context.TodoItems.Add(new TodoItem
-            {
-                Id = Guid.NewGuid(),
-                IsCompleted = false,
-                Description = "Pending 2"
-            });
-            context.SaveChanges();
-
-            var sut = new TodoItemsController(context, Substitute.For<ILogger<TodoItemsController>>());
-
-            var result = await sut.GetTodoItems();
-
-            result.Should().NotBeNull();
-            result.Should().BeOfType<OkObjectResult>();
-
-            var okResult = result as OkObjectResult;
-            okResult.StatusCode.Should().Be(200);
-            okResult.Value.Should().BeAssignableTo<IEnumerable<TodoItem>>();
-
-            var items = okResult.Value as IEnumerable<TodoItem>;
-            items.Should().HaveCount(2)
-                .And.Contain(x => x.Description == "Pending 1")
-                .And.Contain(x => x.Description == "Pending 2");
-        }
+      context.Database.EnsureDeleted();
+      context.Database.EnsureCreated();
+      
+      return context;
     }
+
+    [Fact]
+    public async Task Return_AllIncompleteItems_When_GetTodoItems()
+    {
+      var context = InitContext();
+      context.TodoItems.Add(new TodoItem
+      {
+        Id = Guid.NewGuid(),
+        IsCompleted = false,
+        Description = "Pending 1"
+      });
+      context.TodoItems.Add(new TodoItem
+      {
+        Id = Guid.NewGuid(),
+        IsCompleted = true,
+        Description = "Completed"
+      });
+      context.TodoItems.Add(new TodoItem
+      {
+        Id = Guid.NewGuid(),
+        IsCompleted = false,
+        Description = "Pending 2"
+      });
+      context.SaveChanges();
+
+      var sut = new TodoItemsController(context, Substitute.For<ILogger<TodoItemsController>>());
+
+      var result = await sut.GetTodoItems();
+
+      result.Should().NotBeNull();
+      result.Should().BeOfType<OkObjectResult>();
+
+      var okResult = result as OkObjectResult;
+      okResult.Value.Should().BeAssignableTo<IEnumerable<TodoItem>>();
+
+      var items = okResult.Value as IEnumerable<TodoItem>;
+      items.Should().HaveCount(2)
+          .And.Contain(x => x.Description == "Pending 1")
+          .And.Contain(x => x.Description == "Pending 2");
+    }
+
+    [Fact]
+    public async Task Return_Item_When_GetTodoItem()
+    {
+      var id = Guid.NewGuid();
+      var context = InitContext();
+      context.TodoItems.Add(new TodoItem
+      {
+        Id = Guid.NewGuid(),
+        IsCompleted = false,
+        Description = "Pending 1"
+      });
+      context.TodoItems.Add(new TodoItem
+      {
+        Id = id,
+        IsCompleted = false,
+        Description = "Pending 2"
+      });
+      context.TodoItems.Add(new TodoItem
+      {
+        Id = Guid.NewGuid(),
+        IsCompleted = false,
+        Description = "Pending 3"
+      });
+      context.SaveChanges();
+
+      var sut = new TodoItemsController(context, Substitute.For<ILogger<TodoItemsController>>());
+
+      var result = await sut.GetTodoItem(id);
+
+      result.Should().NotBeNull();
+      result.Should().BeOfType<OkObjectResult>();
+
+      var okResult = result as OkObjectResult;
+      okResult.Value.Should().BeOfType<TodoItem>();
+
+      var item = okResult.Value as TodoItem;
+      item.Id.Should().Be(id);
+      item.Description.Should().Be("Pending 2");
+      item.IsCompleted.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task Return_404_When_GetTodoItem_Given_ItemDoesntExist()
+    {
+      var context = InitContext();
+      context.TodoItems.Add(new TodoItem
+      {
+        Id = Guid.NewGuid(),
+        IsCompleted = false,
+        Description = "Pending 1"
+      });
+      context.TodoItems.Add(new TodoItem
+      {
+        Id = Guid.NewGuid(),
+        IsCompleted = false,
+        Description = "Pending 2"
+      });
+      context.TodoItems.Add(new TodoItem
+      {
+        Id = Guid.NewGuid(),
+        IsCompleted = false,
+        Description = "Pending 3"
+      });
+      context.SaveChanges();
+
+      var sut = new TodoItemsController(context, Substitute.For<ILogger<TodoItemsController>>());
+
+      var result = await sut.GetTodoItem(Guid.NewGuid());
+
+      result.Should().NotBeNull();
+      result.Should().BeOfType<NotFoundResult>();
+
+      var notFoundResult = result as NotFoundResult;
+    }
+  }
 }
