fe609861f46612a9498fe6e121a70ecc55f92e5d
Yana <yana@tkh.nz>
2023-04-09 18:56:44 +1200

    Update api to throw meaninful errors

--- Frontend-React/src/services/__tests__/api.test.js
+++ Frontend-React/src/services/__tests__/api.test.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { createTodoItem, getTodoItems, updateTodoItem } from '../api'
+import { ApiError, createTodoItem, getTodoItems, updateTodoItem } from '../api'
 
 jest.mock('axios')
 
@@ -27,6 +27,26 @@ describe('api should', () => {
     expect(response).toEqual(data)
   })
 
+  it('throw ApiError when createTodoItem given request fails', async () => {
+    const data = { id: '123', description: 'test' }
+    axios.post.mockRejectedValue({ response: { status: 500 } })
+
+    const act = () => createTodoItem(data)
+
+    await expect(act).rejects.toThrow(ApiError)
+    await expect(act).rejects.toThrow('Something went wrong. Please try again.')
+  })
+
+  it('throw ApiError with API message when createTodoItem given request fails with 400', async () => {
+    const data = { id: '123', description: 'test' }
+    axios.post.mockRejectedValue({ response: { status: 400, data: 'Duplicate description' } })
+
+    const act = () => createTodoItem(data)
+
+    await expect(act).rejects.toThrow(ApiError)
+    await expect(act).rejects.toThrow('Duplicate description')
+  })
+
   it('call get with no data when getTodoItems', async () => {
     const data = [
       { id: '123', description: 'test' },
@@ -35,10 +55,19 @@ describe('api should', () => {
     axios.get.mockResolvedValue({ data })
 
     process.env.REACT_APP_API_BASE_URL = 'https://test.api'
-    const response = await getTodoItems(data)
+    const response = await getTodoItems()
 
     expect(axios.get).toHaveBeenCalledWith('https://test.api/api/TodoItems')
     expect(response).toEqual(data)
+  })
+
+  it('throw ApiError when getTodoItems given request fails', async () => {
+    axios.get.mockRejectedValue({ response: { status: 500 } })
+
+    const act = () => getTodoItems()
+
+    await expect(act).rejects.toThrow(ApiError)
+    await expect(act).rejects.toThrow('Something went wrong. Please refresh the list.')
   })
 
   it('call put with data when updateTodoItem', async () => {
@@ -50,5 +79,35 @@ describe('api should', () => {
 
     expect(axios.put).toHaveBeenCalledWith('https://test.api/api/TodoItems/123', data)
     expect(response).toEqual(data)
+  })
+
+  it('throw ApiError when updateTodoItem given request fails', async () => {
+    const data = { id: '123', description: 'test' }
+    axios.put.mockRejectedValue({ response: { status: 500 } })
+
+    const act = () => updateTodoItem(data)
+
+    await expect(act).rejects.toThrow(ApiError)
+    await expect(act).rejects.toThrow('Something went wrong. Please try again.')
+  })
+
+  it('throw ApiError when updateTodoItem given request fails with 404', async () => {
+    const data = { id: '123', description: 'test' }
+    axios.put.mockRejectedValue({ response: { status: 404 } })
+
+    const act = () => updateTodoItem(data)
+
+    await expect(act).rejects.toThrow(ApiError)
+    await expect(act).rejects.toThrow('The item is not found. Please refresh the list.')
+  })
+
+  it('throw ApiError and API message when updateTodoItem given request fails with 400', async () => {
+    const data = { id: '123', description: 'test' }
+    axios.put.mockRejectedValue({ response: { status: 400, data: 'Duplicate description' } })
+
+    const act = () => updateTodoItem(data)
+
+    await expect(act).rejects.toThrow(ApiError)
+    await expect(act).rejects.toThrow('Duplicate description')
   })
 })
--- Frontend-React/src/services/api.js
+++ Frontend-React/src/services/api.js
@@ -1,17 +1,47 @@
 import axios from 'axios'
 
+export class ApiError extends Error {
+  constructor(message) {
+    super(message)
+    this.name = 'ApiError'
+  }
+}
+
 export function getTodoItems() {
-  return axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/TodoItems`).then((response) => response.data)
+  return axios
+    .get(`${process.env.REACT_APP_API_BASE_URL}/api/TodoItems`)
+    .then((response) => response.data)
+    .catch(() => {
+      throw new ApiError('Something went wrong. Please refresh the list.')
+    })
 }
 
 export function updateTodoItem(todoItemModel) {
   return axios
     .put(`${process.env.REACT_APP_API_BASE_URL}/api/TodoItems/${todoItemModel.id}`, todoItemModel)
     .then((response) => response.data)
+    .catch((error) => {
+      switch (error.response.status) {
+        case 400:
+          throw new ApiError(error.response.data)
+        case 404:
+          throw new ApiError('The item is not found. Please refresh the list.')
+        default:
+          throw new ApiError('Something went wrong. Please try again.')
+      }
+    })
 }
 
 export function createTodoItem(todoItemModel) {
   return axios
     .post(`${process.env.REACT_APP_API_BASE_URL}/api/TodoItems`, todoItemModel)
     .then((response) => response.data)
+    .catch((error) => {
+      switch (error.response.status) {
+        case 400:
+          throw new ApiError(error.response.data)
+        default:
+          throw new ApiError('Something went wrong. Please try again.')
+      }
+    })
 }
